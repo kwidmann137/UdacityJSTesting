7687acf5e08197faffe85dbe835524fd81cc0e32
Kyle Widmann <Kyle.Widmann@gmail.com>
2016-12-24 10:16:16 -0600

    Fixed feedreader.js errors

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,65 +27,64 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
          it('all feeds have URL', function(){
             allFeeds.forEach(function(feed){
                 expect(feed.url).toBeDefined();
-                expect(feed.url).not.toBe("");
+                expect(feed.url).not.toBe('');
             });
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
          it('all feeds have name', function(){
             allFeeds.forEach(function(feed){
                 expect(feed.name).toBeDefined();
-                expect(feed.name).not.toBe("");
+                expect(feed.name).not.toBe('');
             });
         });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* A new test suite named 'The menu' */
 
-    describe("The Menu", function(){
-        /* TODO: Write a test that ensures the menu element is
+    describe('The Menu', function(){
+        /* A test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
          it('Is hidden as default', function(){
             var body = $('body');
-            expect(body.hasClass("menu-hidden")).toBe(true);
+            expect(body.hasClass('menu-hidden')).toBe(true);
          });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* A test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
           it('Changes states when clicked', function(){
             var body = $('body');
-            var originalState = body.hasClass("menu-hidden");
             //trigger the click then test
             $('.icon-list').trigger('click');
-            var newState = body.hasClass("menu-hidden");
-            expect(newState).not.toBe(originalState);
+            var isHidden = body.hasClass('menu-hidden');
+            expect(isHidden).toBe(false);
             // now trigget click again and retest
             $('.icon-list').trigger('click');
-            newState = body.hasClass("menu-hidden");
-            expect(newState).toBe(originalState);
+            isHidden = body.hasClass('menu-hidden');
+            expect(isHidden).toBe(true);
          });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-    describe("Initial Entries", function(){
-        /* TODO: Write a test that ensures when the loadFeed
+    /* A new test suite named 'Initial Entries' */
+    describe('Initial Entries', function(){
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -94,21 +93,20 @@ $(function() {
 
          beforeEach(function(done){
             loadFeed(0, function(){
-                done();
+            	done();
             });
-         });
+        });
 
          it('has at least one entry', function(done){
-            var container = $('.feed');
-            var numEntries = container.find('.entry').length;
+            var numEntries = $('.feed .entry').length;
             expect(numEntries).toBeGreaterThan(0);
             done();
          });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection"*/
-    describe("New Feed Selection", function(){
-        /* TODO: Write a test that ensures when a new feed is loaded
+    /* A new test suite named 'New Feed Selection'*/
+    describe('New Feed Selection', function(){
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
@@ -129,7 +127,15 @@ $(function() {
             var newContent = $('.entry h2').map(function(){
                     return $(this).text();
                 }).get();
-            expect(initialContent).not.toBe(newContent);
+            var lengthToCheck;
+            if(newContent.length < initialContent.length){
+            	lengthToCheck = newContent.length;
+            }else{
+            	lengthToCheck = initialContent.length;
+            }
+            for(var i = 0; i < lengthToCheck; i++){
+            	expect(initialContent).not.toBe(newContent);
+            }
             done();
          });
     });
